e10607c709c9452ef0a9c6691bb7424592c125cb
Ash <ashleighb.jjd@gmail.com>
2025-04-01 20:41:04 -0400

    Altered component tests

--- cypress/component/Quiz.cy.tsx
+++ cypress/component/Quiz.cy.tsx
@@ -27,6 +27,8 @@ describe("Quiz Component", () => {
 
     cy.get('button').contains('Start Quiz').click();
     cy.get('button').contains('1').click();
+    cy.get('button').contains('2').click();
+    cy.get('button').contains('4').click();
     cy.get('.alert-success').should('be.visible').and('contain', 'Your score');
 
     
@@ -37,7 +39,13 @@ describe("Quiz Component", () => {
     mount(<Quiz />);
 
     cy.get('button').contains('Start Quiz').click();
-    cy.get('button').contains('1').click();
+    
+    const questionCount = 3;
+    for (let i = 0; i < questionCount; i++) {
+      cy.get('h2').should('exist').and('not.be.empty');
+      cy.get('button').contains('1').click();
+    }
+    
     cy.get('button').contains('Take New Quiz').click();
     cy.get('h2').should('exist').and('not.be.empty');
   });
